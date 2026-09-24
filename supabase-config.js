f91de47c7f08d50dcfaf7cd5bb3d33e8f7ccad3b
Pritish Chandra Ghosh Payel <pritishghosh4769@gmail.com>
2026-09-24 14:37:48 +0600

    PorikkhaHub initial upload

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+﻿const SUPABASE_URL = "https://jsjkxpsnuizngofyjvdl.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_rGBAYVe2ZeqDk8tvrYy2Tg_m4fbtSLf";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
